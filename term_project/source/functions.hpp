bbe9651593cdd26ad0c790405460a0ee210086c1
yuling <asdf168gh@gmail.com>
2016-05-11 20:09:28 +0800

    initial visual studio working space

---
+++ term_project/source/functions.hpp
@@ -0,0 +1,67 @@
+#ifndef FUNCTIONS_HPP_INCLUDED
+#define FUNCTIONS_HPP_INCLUDED
+#include <opencv2\opencv.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <functional>
+
+namespace cp{
+
+//Multi channel 8bit PSNR
+double PSNR_UCHAR(const cv::Mat& input1, const cv::Mat& input2){
+  double diff;
+  double sigma = 0;
+  for (int i = 0; i < input1.rows; i++){
+    const uchar *in1_ptr = input1.ptr(i);
+    const uchar *in2_ptr = input2.ptr(i);
+    for (int j = 0; j < input1.cols; j++){
+      for (int c = 0; c < input1.channels(); c++){
+        diff = (*in1_ptr++) - (*in2_ptr++);
+        sigma += diff*diff;
+      }
+    }
+  }
+  double mse = sigma / input1.rows / input1.cols;
+  if (mse < 1e-100) return 10000;
+  else return 20 * log10(255) - 10 * log10(mse);
+}
+
+// dist = func(src)
+template<typename T>
+void elementWiseOperator(const cv::Mat& src, cv::Mat& dist, const std::function<T(T)>& func){
+  cv::Mat temp(src.rows, src.cols, src.type());
+  for (int i = 0; i < src.rows; i++){
+    const T* ptrs = src.ptr<T>(i);
+    T* ptrd = temp.ptr<T>(i);
+    for (int j = 0; j < src.cols; j++){
+      for (int c = 0; c < src.channels(); c++){
+        *ptrd++ = func(*ptrs++);
+      }
+    }
+  }
+  dist = temp;
+}
+
+// dist = func(src1, src2)
+template<typename T>
+void elementWiseOperator(const cv::Mat& src1, const cv::Mat& src2, cv::Mat& dist, const std::function<T(T, T)>& func){
+  assert(src1.type() == src2.type());
+  cv::Mat temp(src1.rows, src1.cols, src1.type());
+  for (int i = 0; i < src1.rows; i++){
+    const T* ptrs1 = src1.ptr<T>(i);
+    const T* ptrs2 = src2.ptr<T>(i);
+    T* ptrd = temp.ptr<T>(i);
+    for (int j = 0; j < src1.cols; j++){
+      for (int c = 0; c < src1.channels(); c++){
+        *ptrd++ = func(*ptrs1++, *ptrs2++);
+      }
+    }
+  }
+  dist = temp;
+}
+
+}
+
+#endif // FUNCTIONS_HPP_INCLUDED
