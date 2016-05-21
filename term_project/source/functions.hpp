1b42463eeaf31ea970a25005d787714feca5eb35
yuling <asdf168gh@gmail.com>
2016-05-21 15:17:03 +0800

    use ++i instead of i++

--- term_project/source/functions.hpp
+++ term_project/source/functions.hpp
@@ -14,11 +14,11 @@ namespace cp{
 double PSNR_UCHAR(const cv::Mat& input1, const cv::Mat& input2){
   double diff;
   double sigma = 0;
-  for (int i = 0; i < input1.rows; i++){
+  for (int i = 0; i < input1.rows; ++i){
     const uchar *in1_ptr = input1.ptr(i);
     const uchar *in2_ptr = input2.ptr(i);
-    for (int j = 0; j < input1.cols; j++){
-      for (int c = 0; c < input1.channels(); c++){
+    for (int j = 0; j < input1.cols; ++j){
+      for (int c = 0; c < input1.channels(); ++c){
         diff = (*in1_ptr++) - (*in2_ptr++);
         sigma += diff*diff;
       }
@@ -33,11 +33,11 @@ double PSNR_UCHAR(const cv::Mat& input1, const cv::Mat& input2){
 template<typename T>
 void elementWiseOperator(const cv::Mat& src, cv::Mat& dist, const std::function<T(T)>& func){
   cv::Mat temp(src.rows, src.cols, src.type());
-  for (int i = 0; i < src.rows; i++){
+  for (int i = 0; i < src.rows; ++i){
     const T* ptrs = src.ptr<T>(i);
     T* ptrd = temp.ptr<T>(i);
-    for (int j = 0; j < src.cols; j++){
-      for (int c = 0; c < src.channels(); c++){
+    for (int j = 0; j < src.cols; ++j){
+      for (int c = 0; c < src.channels(); ++c){
         *ptrd++ = func(*ptrs++);
       }
     }
@@ -50,12 +50,12 @@ template<typename T>
 void elementWiseOperator(const cv::Mat& src1, const cv::Mat& src2, cv::Mat& dist, const std::function<T(T, T)>& func){
   assert(src1.type() == src2.type());
   cv::Mat temp(src1.rows, src1.cols, src1.type());
-  for (int i = 0; i < src1.rows; i++){
+  for (int i = 0; i < src1.rows; ++i){
     const T* ptrs1 = src1.ptr<T>(i);
     const T* ptrs2 = src2.ptr<T>(i);
     T* ptrd = temp.ptr<T>(i);
-    for (int j = 0; j < src1.cols; j++){
-      for (int c = 0; c < src1.channels(); c++){
+    for (int j = 0; j < src1.cols; ++j){
+      for (int c = 0; c < src1.channels(); ++c){
         *ptrd++ = func(*ptrs1++, *ptrs2++);
       }
     }
@@ -115,15 +115,15 @@ void paddingWithReplicate(const cv::Mat& src, int paddingSize, cv::Mat& dist){
   dist.create(src.rows + 2 * paddingSize, src.cols + 2 * paddingSize, src.type());
   const T* ptrs;
   T* ptrd;
-  for (int i = 0; i < dist.rows; i++){
+  for (int i = 0; i < dist.rows; ++i){
     ptrd = dist.ptr<T>(i);
     // point to source Mat
     if (i - paddingSize < 0) { ptrs = src.ptr<T>(0); }
     else if (i - paddingSize >= src.rows) { ptrs = src.ptr<T>(src.rows - 1); }
     else { ptrs = src.ptr<T>(i - paddingSize); }
 
-    for (int j = 0; j < dist.cols; j++){
-      for (int c = 0; c < dist.channels(); c++){
+    for (int j = 0; j < dist.cols; ++j){
+      for (int c = 0; c < dist.channels(); ++c){
         ptrd[c] = ptrs[c];
       }
       ptrd += dist.channels();
@@ -176,9 +176,9 @@ void getGaussianKernel(int height, int width, double sigmaS, cv::Mat& kernel){
   int widthRef = width / 2;
   T* ptrk;
   T sum = 0.0;
-  for (int i = 0; i < height; i++){ // construct 2D gaussian kernel
+  for (int i = 0; i < height; ++i){ // construct 2D gaussian kernel
     ptrk = kernel.ptr<T>(i);
-    for (int j = 0; j < width; j++){
+    for (int j = 0; j < width; ++j){
       *ptrk = exp(-(pow(i - heightRef, 2) + pow(j - widthRef, 2)) / (2 * pow(sigmaS, 2)));
       sum += *ptrk;
       ptrk++;
@@ -204,7 +204,7 @@ void GaussianFilter(const cv::Mat& src, cv::Mat& dist, int windowSize, double si
     int wy_tmp = (wy >= 0) ? wy : -wy;
     weightSum += (weight[wy_tmp]);
   }
-  for (int i = 0; i <= step; i++)
+  for (int i = 0; i <= step; ++i)
     weight[i] = weight[i] / weightSum;
 
   // compute 2-D Gaussian filter with 1-D separatable Gaussian filter, O(n)
@@ -255,17 +255,17 @@ void bilateralFilter(const cv::Mat& src, int windowSize, double sigmaS, double s
   const T* ptrp; // point to padding image
   const T* ptrpr; // point to center point
   T sum, norm, mul;
-  for (int i = 0; i < dist.rows; i++){
+  for (int i = 0; i < dist.rows; ++i){
     ptrd = dist.ptr<T>(i);
-    for (int j = 0; j < dist.cols; j++){
-      for (int c = 0; c < dist.channels(); c++){
+    for (int j = 0; j < dist.cols; ++j){
+      for (int c = 0; c < dist.channels(); ++c){
         sum = 0.0;
         norm = 0.0;
         ptrpr = padding.ptr<T>(i + paddingSize, j + paddingSize);
-        for (int ki = 0; ki < windowSize; ki++){
+        for (int ki = 0; ki < windowSize; ++ki){
           ptrp = padding.ptr<T>(i + ki, j);
           ptrgk = gKernel.ptr<T>(ki);
-          for (int kj = 0; kj < windowSize; kj++){
+          for (int kj = 0; kj < windowSize; ++kj){
             mul = (*ptrgk) * exp(-(pow(ptrp[c] - ptrpr[c], 2)) / (2 * pow(sigmaR, 2))); // update kernel to bilateral
             sum += mul * ptrp[c];
             norm += mul;
